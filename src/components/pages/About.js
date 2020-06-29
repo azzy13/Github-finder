cd73bf944f42204aa7ad8a32028d50fca5ce68fb
azzy13 <azharhasan013@gmail.com>
2020-06-29 18:42:00 +0530

    Development Successful

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this page</h1>
+      <p>App to search Github Users via Username</p>
+      <p>Version: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
